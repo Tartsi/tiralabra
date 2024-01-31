e8d4eb199f3dbb8e9fd6ffcdca75ab5100d1c9ac
Tartsi <tarvo.lilja@hotmail.fi>
2024-01-31 20:16:59 +0200

    Add undo_move for minimax

--- src/services/board.py
+++ src/services/board.py
@@ -12,20 +12,30 @@ class Board:
 
         self.board = [['-' for _ in range(board_size)]
                       for _ in range(board_size)]
+        # Necessary for MiniMax-algorithm
+        self.moves_made = []
 
     def make_move(self, row, column, player):
         """Moves the player on the board if given square is not occupied"""
 
         if self.board[row][column] == '-':
+            current_state = self.board[row][column]
+            self.moves_made.append((row, column, current_state))
             self.board[row][column] = player
             return True
 
         return False
 
-    # def undo_move(self, row, column, player):
-    #     """Undos the previously made move on the board
-    #     Necessary for AI's minimax-algorithm"""
-    #     pass
+    def undo_move(self):
+        """Undos the previously made move on the board
+        Necessary for AI's minimax-algorithm"""
+
+        if self.moves_made:
+            row, column, previous_state = self.moves_made.pop()
+            self.board[row][column] = previous_state
+            return True
+
+        return False
 
     def get_possible_moves(self):
         """Returns a set of possible moves"""
--- src/tests/board_test.py
+++ src/tests/board_test.py
@@ -11,17 +11,34 @@ class TestBoard(unittest.TestCase):
 
     def test_set_up_board_default(self):
         self.assertEqual(len(self.board.board), (20))
+        self.assertEqual(len(self.board.moves_made), 0)
 
     def test_set_up_board_with_given_value(self):
         board = Board(5)
         self.assertEqual(len(board.board), (5))
+        self.assertEqual(len(self.board.moves_made), 0)
 
     def test_make_legal_move(self):
         self.assertEqual(self.board.make_move(1, 1, "0"), True)
+        self.assertEqual(self.board.moves_made[0], (1, 1, "-"))
 
     def test_make_illegal_move(self):
         self.board.make_move(1, 1, "0")
         self.assertFalse(self.board.make_move(1, 1, "0"))
+        self.assertEqual(self.board.moves_made[0], (1, 1, "-"))
+
+    def test_undo_move_empty_board(self):
+        self.assertFalse(self.board.undo_move())
+
+    def test_undo_move_after_legal_move(self):
+        self.board.make_move(1, 1, "0")
+        self.assertTrue(self.board.undo_move())
+
+    def test_undo_move_after_illegal_move(self):
+        self.board.make_move(1, 1, "0")
+        self.board.make_move(1, 1, "0")
+        self.assertTrue(self.board.undo_move())
+        self.assertFalse(self.board.undo_move())
 
     def test_board_print_empty(self):
         sys.stdout = io.StringIO()
